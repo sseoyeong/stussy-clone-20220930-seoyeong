3fc5f4b16eb5782af22ce30721c9d2f54a4df11c
sseoyeong <ssss0012ss@naver.com>
2022-10-25 16:12:12 +0900

    페이지 스크롤 처리

--- src/main/java/com/stussy/stussyclone20220930seoyeong/repository/ProductRepository.java
+++ src/main/java/com/stussy/stussyclone20220930seoyeong/repository/ProductRepository.java
@@ -5,8 +5,9 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface ProductRepository {
-    public List<CollectionsProduct> getProductList(HashMap<String, Object> map) throws Exception;
+    public List<CollectionsProduct> getProductList(Map<String, Object> map) throws Exception;
 }
